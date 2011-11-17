593d77f86bd5a6ee670f41cd45d0fd2119306e0d
winhamwr <winhamwr@gmail.com>
2011-11-17 11:32:26 -0500

    Added a dashboard view using django-dev-dashboard code as a base

---
+++ dbag/static/dbag/js/dashboard.js
@@ -0,0 +1,50 @@
+if (typeof DBAG_BASE_URL === 'undefined') {
+    DBAG_BASE_URL = "/dbag/";
+}
+$(function () {
+    $("div.metric div.sparkline").each(function (index, elem) {
+        var e = $(elem),
+            value_element = e.parent().find('p.value a'),
+            timestamp_element = e.parent().find('span.timestamp'),
+            original_value = value_element.html(),
+            i;
+
+        var url = DBAG_BASE_URL + 'metric/' + e.data('metric') + ".json";
+        $.getJSON(url, function (response) {
+            // flot time series data needs to be in time/value pairs and the
+            // time needs to be in *milliseconds*, not seconds.  fixing this in
+            // Python would be easier but would limit reuse.
+            js_data = [];
+            for (i = 0; i < response.data.length; i++) {
+                var datapoint = [];
+                datapoint[0] = response.data[i].utc_timestamp * 1000;
+                datapoint[1] = response.data[i].value;
+                js_data.push(datapoint);
+            }
+            var options = {
+                xaxis: {show: false, mode: "time"},
+                yaxis: {show: false, min: 0},
+                grid: {borderWidth: 0, hoverable: true},
+                colors: ["yellow"]
+            };
+            $.plot(e, [js_data], options);
+
+            e.bind('plothover', function (event, pos, item) {
+                if (item) {
+                    var d = new Date(item.datapoint[0]);
+                    timestamp_element.html(
+                        $.plot.formatDate(d, "%b %d, %h:%M%p")
+                    );
+                    value_element.html(item.datapoint[1]);
+                } else {
+                    value_element.html(original_value);
+                    timestamp_element.html('&nbsp;');
+                }
+            });
+        });
+
+        e.click(function () {
+            window.location = DBAG_BASE_URL + "metric/" + e.data('metric') + '/';
+        });
+    });
+});
--- dbag/views.py
+++ dbag/views.py
@@ -1,1 +1,62 @@
+import datetime
+import time
+
+from django import http
+from django.forms.models import model_to_dict
+from django.shortcuts import render, get_object_or_404
+from django.utils import simplejson
+#from django.views.decorators.cache import cache_page
+
+from dbag.models import Metric
+
+#@cache_page(60 * 10) # 10 minutes
+def index(request):
+    metrics = Metric.objects.all().order_by('label')
+
+    data = []
+    for metric in metrics:
+        latest = metric.get_latest_sample()
+        data.append({'metric': metric, 'latest': latest})
+
+    return render(request, 'dbag/index.html', {'data': data})
+
+def metric_detail(request, metric_slug):
+    metric = get_object_or_404(Metric, slug=metric_slug)
+
+    return render(
+        request,
+        'dbag/detail.html',
+        {
+            'metric': metric,
+            'latest': metric.get_latest_sample(),
+        }
+    )
+
+#@cache_page(60 * 10) # 10 minutes
+def metric_json(request, metric_slug):
+    metric = get_object_or_404(Metric, slug=metric_slug)
+
+    try:
+        daysback = int(request.GET['days'])
+    except (TypeError, KeyError):
+        daysback = 30
+
+    start_date = datetime.datetime.utcnow() - datetime.timedelta(days=daysback)
+
+    metric_dict = model_to_dict(metric)
+    metric_dict['data'] = []
+    for sample in metric.get_samples_since(utc_datetime=start_date):
+        metric_dict['data'].append(
+            {
+                'value': sample.value,
+                'utc_timestamp': time.mktime(sample.utc_timestamp.timetuple()),
+            }
+        )
+
+    return http.HttpResponse(
+        simplejson.dumps(metric_dict, indent=2),
+        content_type="application/json")
+
+
+
 
